dffc3766e5f60e2dfd2c7b0b243547041023c0a0
AEnterprise <aenterprise2@gmail.com>
2020-05-03 19:57:53 +0200

    move the database stuff

--- src/core/context/database.rs
+++ src/core/context/database.rs
@@ -1,4 +1,5 @@
 use crate::core::{Context, GuildConfig};
+use crate::database::guild::get_guild_config;
 use crate::utils::Error;
 use dashmap::mapref::one::Ref;
 use log::{debug, info};
@@ -13,37 +14,7 @@ impl Context {
         match self.configs.get(&guild_id) {
             Some(config) => Ok(config),
             None => {
-                let client = self.pool.get().await?;
-                let statement = client
-                    .prepare_typed("SELECT config from guildconfig where id=$1", &[Type::INT8])
-                    .await?;
-
-                let rows = client.query(&statement, &[&(guild_id.0 as i64)]).await?;
-
-                let config: GuildConfig = if rows.is_empty() {
-                    let config = GuildConfig::default();
-                    info!("No config found for {}, inserting blank one", guild_id);
-                    let statement = client
-                        .prepare_typed(
-                            "INSERT INTO guildconfig (id, config) VALUES ($1, $2)",
-                            &[Type::INT8, Type::JSON],
-                        )
-                        .await?;
-                    client
-                        .execute(
-                            &statement,
-                            &[
-                                &(guild_id.0 as i64),
-                                &serde_json::to_value(&GuildConfig::default()).unwrap(),
-                            ],
-                        )
-                        .await?;
-
-                    config
-                } else {
-                    serde_json::from_value(rows[0].get(0))?
-                };
-
+                let config = get_guild_config(&self.pool, guild_id.0).await?;
                 self.configs.insert(guild_id, config);
                 Ok(self.configs.get(&guild_id).unwrap())
             }
